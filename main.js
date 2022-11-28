c71c3bf2c35d4932c788227e3a0afc055c7ed3ef
Ramin <ramin.parvanhe98@gmail.com>
2022-11-28 20:56:40 +0330

    add all

---
+++ main.js
@@ -0,0 +1,84 @@
+
+var getFirst = document.getElementById('first')
+var getSecond = document.getElementById('second')
+var getThree = document.getElementById('third')
+var getForth = document.getElementById('forth')
+var getFifth = document.getElementById('fifth')
+var getSixth = document.getElementById('sixth')
+var getSeventh = document.getElementById('seventh')
+
+
+
+
+
+function showone() {
+ getFirst.style.display = "none";
+}
+
+function showtwo() {
+  getSecond.style.display = 'none';
+}
+
+function showthree() {
+  getThree.style.display = 'none';
+
+}
+
+function showforth() {
+  getForth.style.display = 'none';
+}
+
+function showfifth() {
+  getFifth.style.display = 'none';
+}
+function showsixth() {
+  getSixth.style.display = 'none';
+}
+
+function showseventh() {
+  getSeventh.style.display = 'none';
+}
+
+///over
+
+function overOne() {
+  getFirst.style.display = 'block';
+}
+
+
+
+
+function overtwo() {
+  getSecond.style.display = 'block';
+}
+
+
+
+function overthree() {
+  getThree.style.display = 'block';
+}
+
+
+function overfour() {
+  getForth.style.display = 'block';
+}
+
+
+function overfive() {
+  getFifth.style.display = 'block';
+}
+
+
+
+function oversixth() {
+  getSixth.style.display = 'block';
+}
+
+
+function overseventh() {
+  getSeventh.style.display = 'block';
+}
+
+
+
+
